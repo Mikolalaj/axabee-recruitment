fed962f39ad5206c0409dca33f9d30d2138a06ef
Mikolaj Olejnik <olejnikmikolaj@gmail.com>
2022-07-14 19:17:46 +0200

    working prototype

--- index.ts
+++ index.ts
@@ -1,12 +1,47 @@
 import 'dotenv/config'
 import express, { Express, Request, Response } from 'express';
+import { createToken, verifyToken } from './utils.js';
 
-
-const app: Express = express();
 const port = process.env.PORT || 8000;
 
+const app: Express = express();
+
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    res.send('Available endpoints: /api/auth and /api/secret');
+});
+
+app.post('/api/auth', (req: Request, res: Response) => {
+    const email = req.body.email as string;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (emailRegex.test(email)) {
+        const token = createToken(email);
+        res.status(200).json({
+            'message': 'Authenticated',
+            'token': token
+        });
+    }
+    else {
+        res.status(400).json({ 'message': 'Invalid email' });
+    }
+});
+
+app.get('/api/secret', (req: Request, res: Response) => {
+    const bearerToken = req.headers.authorization as string;
+    const token = bearerToken.replace('Bearer ', '');
+
+    try {
+        verifyToken(token)
+        res.status(200).json({
+            'message': 'Authenticated',
+            'secret': process.env.SECRET
+        });
+    } catch (error) {
+        res.status(400).json({ 'message': 'Invalid token' });
+    }
+
 });
 
 app.listen(port, () => {
---
+++ utils.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+function createToken(email: string) {
+    return jwt.sign(
+        { email },
+        process.env.SECRET_JWT,
+        { algorithm: 'HS256', expiresIn: '24h' }
+    );
+}
+
+function verifyToken(token: string) {
+    return jwt.verify(token, process.env.SECRET_JWT, { algorithms: ['HS256'] });
+}
+
+export {
+    createToken,
+    verifyToken
+};
